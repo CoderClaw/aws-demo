3542e1213f24a2950b55a2a515fe03104ef83026
coderclaw <coderclaw@gmail.com>
2023-01-07 23:49:12 -0300

    amplify setup

--- amplify-app/src/app/app.component.spec.ts
+++ amplify-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'aws-demo'`, () => {
+  it(`should have as title 'amplify-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('aws-demo');
+    expect(app.title).toEqual('amplify-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('aws-demo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('amplify-app app is running!');
   });
 });
---
+++ amplify-app/src/polyfills.ts
@@ -0,0 +1,32 @@
+import 'core-js/es/typed-array';
+import 'core-js/es/object';
+
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
+
+(global as any).document = window.document;
+(global as any).Event = window.Event;
+(global as any).KeyboardEvent = window.KeyboardEvent;
+(global as any).MouseEvent = window.MouseEvent;
+(global as any).FocusEvent = window.FocusEvent;
+(global as any).PointerEvent = window.PointerEvent;
+(global as any).HTMLElement = window.HTMLElement;
+(global as any).HTMLElement.prototype.getBoundingClientRect = () => {
+    return {
+      left: '',
+      right: '',
+      top: '',
+      bottom: ''
+  };
+};
+
+// If using IgxIconService to register icons
+(global as any).XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+// Other optional depending on your application configuration
+(global as any).object = window.Object;
+(global as any).navigator = window.navigator;
+(global as any).localStorage = window.localStorage;
+(global as any).DOMTokenList = window.DOMTokenList;
